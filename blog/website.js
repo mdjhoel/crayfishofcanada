df5b86b3607fb682f5e97582f10009b4fb852f53
Mark Hoel <mdjhoel@gmail.com>
2019-01-24 19:04:37 -0500

    Add files via upload

---
+++ blog/website.js
@@ -0,0 +1,67 @@
+function loadJSON(callback) {   
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    //xobj.open('GET', 'https://raw.githubusercontent.com/hoelfamily/website/master/data/blog.json', true);
+    xobj.open('GET', 'http://localhost:8000/blog/data/blog.json', true);
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+          }
+    };
+    xobj.send(null);  
+ }
+
+ function init() {
+ 	loadJSON(function(response) {
+  	
+  		// Parse JSON string into object
+    	var actual_JSON = JSON.parse(response);
+    	var url = window.location.href;
+    	var n = url.indexOf("?");
+    	if (n != -1) { n = url.substr(n+6) }
+    
+    	var slab = [];
+    	slab.push("<header><title>" + actual_JSON[n].name + "</title>");
+    	slab.push("<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'>");
+    	slab.push("<link href='../website.css' rel='stylesheet'>");
+        slab.push("<meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1'>");
+    	slab.push("</header>");
+    	slab.push("<nav class='navbar navbar-inverse navbar-fixed-top' role='navigation'><div class='container'><a class='navbar-brand' href='../index.html'><img alt='Brand' width='75px' src='crayfish.png'></a><ul class='nav navbar-nav navbar-right' style='padding-top: 40px;'><a id='mylink' href='../about.html'>About</a> | <a id='mylink' href='../gallery.html'>Gallery</a> | <a id='mylink' href='../papers.html'>Papers</a> | <a id='mylink' href='index.html'>Blog</a></ul></div></nav>");
+    	slab.push("<div class='container'>");
+    	slab.push("<h3>" + actual_JSON[n].name + "</h3>");
+        slab.push("<p><i class='icon-large icon-search'></i><span class='glyphicon glyphicon-time'></span> Posted by " + actual_JSON[n].author + " on " + actual_JSON[n].date + "</p><hr>");
+    	
+        for (i in actual_JSON[n].segments) {
+    		if (actual_JSON[n].segments[i].img != "") { 
+    			slab.push("<a href=data/images/" + actual_JSON[n].segments[i].img + " target='_blank'>");
+    			slab.push("<img src=data/images/" + actual_JSON[n].segments[i].img + " width='100%'></a>"); 
+    			slab.push("<figcaption><i>" + actual_JSON[n].segments[i].caption + "</i></figcaption>");
+    			slab.push("</figure>");
+    			slab.push("<hr>");
+    		}
+    		if (actual_JSON[n].segments[i].paragraph != "") { 
+    			slab.push("<p>" + actual_JSON[n].segments[i].paragraph + "</p>");
+    			slab.push("<hr>");
+    		}   		
+    	}
+        
+        var keywords = actual_JSON[n].keywords.split(",");
+        slab.push("<div style='padding-bottom:30px;'>");
+        for (i in keywords) {
+            //slab.push("<span class='label label-primary'>" + keywords[i] + "</span>  ");
+            slab.push("<span id='keyword'><div class='label label-primary'>" + keywords[i] + "</div></span>");
+        }
+        slab.push("</div>");
+
+    	slab.push("<footer><div>");
+ 		slab.push("<p>Copyright &copy; Crayfish of Canada 2019</p>");
+        slab.push("</div></footer>");
+
+    	slab.push("</div>");
+
+    	var myhtml = slab.join("");
+    	document.write(myhtml);
+
+ 	});
+ }
---
+++ site.js
@@ -0,0 +1,90 @@
+var app = angular.module('instantsearch',[]);
+
+app.config(function ($httpProvider) {
+  $httpProvider.defaults.headers.common = {};
+  $httpProvider.defaults.headers.post = {};
+  $httpProvider.defaults.headers.put = {};
+  $httpProvider.defaults.headers.patch = {};
+  $httpProvider.defaults.headers.get = {};
+});
+ 
+app.controller('instantSearchCtrl',function($scope,$http){
+	$http.get('https://raw.githubusercontent.com/hoelfamily/website/master/data/blog.json').success(function(data, status, headers, config) {
+		$scope.items = data.reverse();
+
+	}).error(function(data, status, headers, config) {
+		console.log("No data found..");
+  });
+});
+ 
+app.filter('searchFor', function(){
+	return function(arr, searchString){
+		if(!searchString){
+			return arr;
+		}
+		var result = [];
+		searchString = searchString.toLowerCase();
+		
+		angular.forEach(arr, function(item){
+			if(item.keywords.toLowerCase().indexOf(searchString) !== -1){
+				result.push(item);
+			}
+		});
+
+		return result;
+	};
+});
+
+function loadJSON(callback) {   
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'https://raw.githubusercontent.com/hoelfamily/website/master/data/blog.json', true);
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+          }
+    };
+    xobj.send(null);  
+ }
+
+function getKeywords() {
+ loadJSON(function(response) {
+  // Parse JSON string into object
+    var actual_JSON = JSON.parse(response);
+    var doc = document.getElementById("keywords");
+    var keywords = "";
+
+    // create Dictionary object and variable to place trimmed value
+    var dict = new Object;
+    var trimmedkey = "";
+
+    for (i in actual_JSON) {
+      temparray = actual_JSON[i].keywords.split(",");
+      for (j in temparray) {
+      	trimmedkey = temparray[j].trim(); // trim off any spaces
+	if (keywords.indexOf(trimmedkey) == -1) {
+          keywords = keywords + trimmedkey;
+          dict[trimmedkey] = 1;
+	 } else {
+	   dict[trimmedkey] = dict[trimmedkey] + 1;
+	 }
+      }
+    }
+
+    // Sort the keys for further visual confirmation of ranking
+    // Thanks SO - https://stackoverflow.com/questions/1069666/sorting-javascript-object-by-property-value
+    //var sorted_keys = Object.keys(dict).sort();
+    var sorted_keys = Object.keys(dict).sort(function(a,b){return dict[b]-dict[a]});    
+
+    keywords = "";
+    for (var key in sorted_keys) { // go through sorted keys, but get associated count data from original dictionary
+      if (sorted_keys.hasOwnProperty(key)) {
+      	keywords = keywords + "<span id='keyword'><div class='label label-primary'>" + sorted_keys[key] + "   <span class='badge' style='background-color: white; color:black;'>" + dict[sorted_keys[key]] + "</span></div></span>" 
+      } 
+    }
+
+    doc.innerHTML = keywords;
+ });
+ 
+}
